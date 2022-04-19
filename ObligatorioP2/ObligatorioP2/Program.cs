c22200220666dde5edd59b960e4f9697024b6ecf
flopi <villar.florencia@gmail.com>
2022-04-19 15:45:38 -0300

    commit flopi

--- ObligatorioP2/ObligatorioP2/Program.cs
+++ ObligatorioP2/ObligatorioP2/Program.cs
@@ -7,6 +7,8 @@ namespace ObligatorioP2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Commit flopi!");
+
         }
     }
 }
